99ee6f8530b69687ea54bf42869f04572218794f
Bobby DeVeaux <bobby@twistsupport.com>
2014-02-28 22:08:48 +0000

    adding images and javascript aside

---
+++ public_html/inc/orderaside.php
@@ -0,0 +1,22 @@
+<aside class="clearfix">
+    <div class="order_summary clearfix">
+        <h4>Order Summary</h4>
+        <div id="shippingcart" class="clearfix">
+            <div id="shippingproducts"></div>
+            <ul class="total clearfix">
+                <li>Cart Total</li>
+                <li>Shipping</li>
+                <li>Total</li>
+            </ul>
+            <ul class="total clearfix">
+                <li id="subtotal">0</li>
+                <li id="shippingtotal">0</li>
+                <li id="grandtotal">0</li>
+            </ul>
+        </div>
+        <ul class="support clearfix">
+            <li><a id="support" href="#">Customer Support <span></span></a></li>
+            <li><a id="guarantee" href="#">Money Back Guarantee <span></span></a></li>
+        </ul>
+    </div> <!-- order_summary -->
+</aside>
